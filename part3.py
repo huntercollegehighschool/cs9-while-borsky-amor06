3e326dd8062754697778bcf332f3a6eaeac4356b
Brian Gorsky <94186673+borsky-amor06@users.noreply.github.com>
2021-12-25 19:14:33 +0000

    I first made"timestoprint" the integer number amount that the user inputs. I then created a "for i in range (timestoprint)"in order to create a sequence of integers that correlates with the one that the user inputted, which would result in the word "Hunter" being printed that many times.

--- part3.py
+++ part3.py
@@ -14,14 +14,12 @@ Enter a number or enter 0 to stop: 0
 Average: 6.0
 
 '''
-num = int(input("Enter a number or enter 0 to stop: "))
+num = int(input("Enter a number or enter 0 to stop:"))
 total = 0
 totalnum = 0
-
 while num != 0:
   total = total + num
   totalnum = totalnum + 1
-  num = int(input("Enter a number or enter 0 to stop: "))
-
-brian = total / totalnum    
+  num = int(input("Enter a number or enter 0 to stop:"))
+brian = total/totalnum   
 print("Average:", brian)
